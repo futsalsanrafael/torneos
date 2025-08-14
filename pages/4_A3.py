9781a20dedd4713013878eea92ad83e1f90badbc
Gaston <gaston@Gastons-MacBook-Pro.local>
2025-08-14 02:12:12 -0300

    de toque

--- pages/4_A3.py
+++ pages/4_A3.py
@@ -17,10 +17,12 @@ logger = logging.getLogger(__name__)
 
 # Set page configuration
 st.set_page_config(
-    page_title="Futsal De Toque - A3",
-    page_icon=":material/sports_soccer:",
+    page_title="Futsal De Toque",
+    page_icon="images/favicon.jpg",
     layout="wide"
 )
+
+st.logo("images/round.png")
 st.markdown("# A3")
 st.sidebar.markdown("# Futsal De Toque")
 
